722f88ac80f8da8777835bc1fc77e196e64e7392
chenliangyuan <1030868374@qq.com>
2021-05-17 00:29:18 +0800

    feat: init

---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hero.png
+heroText: 学习1
+tagline: 学习1
+actionText: 溜溜 →
+actionLink: ''
+features:
+- title: 学习1
+  details: 专注学习
+- title: 学习2
+  details: 专注学习
+- title: 学习
+  details: 专注学习
+footer: MIT Licensed | Copyright © 2018-present CLY
+---
---
+++ docs/config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  title: '陈良源-cly',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Js', link: '/js/' },
+      { text: 'Node', link: '/node/' },
+      { text: 'Java', link: '/java/' },
+    ],
+    sidebar: {
+      '/js/': getJsSidebar('指南', '深入'),
+      '/node/': getNodeSidebar('指南', '深入')
+    }
+  }
+}
+
+function getJsSidebar (groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [
+        '',
+        'test',
+      ]
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      children: [
+        '',
+      ]
+    }
+  ]
+}
+
+function getNodeSidebar (groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [
+        '',
+        '',
+      ]
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      children: [
+        '',
+      ]
+    }
+  ]
+}
